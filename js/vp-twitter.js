73ae5aca1931e33f43100d8a070807af54b2a95f
Antti Rintala <antti.rintala@gmail.com>
2012-03-12 15:48:32 +0200

    Good stuff

--- js/vp-twitter.js
+++ js/vp-twitter.js
@@ -4,32 +4,38 @@ $(document).ready(function() {
         $feeds = $vp.find("#vp-feed"),
         width = $container.width(),
         height = $container.height(),
-        url = 'http://search.twitter.com/search.json?q=hsopen&rpp=5&include_entities=true&with_twitter_user_id=true&result_type=mixed';
+        url = 'http://search.twitter.com/search.json?q=hsopen&rpp=5&include_entities=true&with_twitter_user_id=true&result_type=mixed',
+        iterator = 0;
+
     var renderResults = function($column1, $column2, data) {
         for(i in data.results) {
             var res = data.results[i],
-                $vpfi = $('<div class="vp-feed-item">' + res.text + '</div>');;
-            if(i % 2 == 0) {
+                date = new Date(res.created_at),
+                $vpfi = $('<div class="vp-feed-item"><div class="vp-feed-user"><strong>' + res.from_user_name + '</strong></div><div>' + res.text + '</div><div class="vp-feed-date">' + date.getDate() + '</div></div>');
+            if(iterator % 2 == 0) {
                 $column1.append($vpfi);
             } else {
                 $column2.append($vpfi);
             }
+            iterator++;
         }
     }
-    
+
     $.getJSON(url + '&callback=?', function(data) {
         $container.append($vp);
+        $vp.css('margin-top', -height + 'px');
         $vp.height(height);
         $feeds.height(height-85);
-        $vp.css('margin-top', -height + 'px');
+        $vp.animate({ 'opacity': 1 });
         var $column1 = $vp.find("#column1").first(),
             $column2 = $vp.find("#column2").first();
-        
+            console.log(data);
         renderResults($column1, $column2, data);
-        
         $.getJSON(url + data.results.next_page + '&callback=?', function(data) {
             renderResults($column1, $column2, data);
+            $.getJSON(url + data.results.next_page + '&callback=?', function(data) {
+                renderResults($column1, $column2, data);
+            });
         });
-        
     });
 });
