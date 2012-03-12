da7899a36fa00a85d5b9ebbb5b40818b42440852
Heikki Uljas <huljas@gmail.com>
2012-03-12 16:17:08 +0200

    Added tweet markers

--- js/vp-chart.js
+++ js/vp-chart.js
@@ -11,44 +11,90 @@ $(function() {
 
     console.log("w=" + width + ",h=" + height);
 
+    var ctx;
     if (canvasObj.getContext) {
         ctx = canvasObj.getContext("2d");
 
         if (ctx) {
-        	ctx.strokeStyle = "#333";
-    		ctx.lineWidth = 2;
-    		
-    		ctx.beginPath();
-
-    		var dx = width / data.length;
-
-    		var x = 0;	
-
-    		var max = 0;
-    		for (i in data) {
-    			if (data[i] > max) {
-    				max = data[i];
-    			}
-    		}	
-    		var ratio = (height-2) / max;
-
-    		for (i in data) {
-    			var y = height - data[i] * ratio;
-    			if (i == 0) {
-    				ctx.moveTo(x, height-data[i]*ratio);
-    			} else {
-    				ctx.lineTo(x, height-data[i]*ratio);		
-    			}
-    			console.log("p(" + x + "," + y + ")");
-    			x = x+dx;    			
-    		}
-			ctx.stroke();
+        	
+        	drawChart(data, ctx);
+        	
         }
     }
 
+    function drawChart(data) {
+		ctx.strokeStyle = "#333";
+		ctx.lineWidth = 2;
+		
+		ctx.beginPath();
 
+		var dx = width / data.length;
+
+		var x = 0;	
+
+		var max = 0;
+		for (i in data) {
+			if (data[i] > max) {
+				max = data[i];
+			}
+		}	
+		var ratio = (height-2) / max;
+
+		var timeslot = dx * 3;
+		for (var i = 0; i * timeslot < width; i++) {
+			if (i % 2 == 0) 
+				ctx.fillStyle = "#eee";
+			else
+				ctx.fillStyle = "#fff";
+			ctx.fillRect(i*timeslot, 0, (i+1)*timeslot, height);
+		}
+
+		var y = 0;
+		for (i in data) {
+			y = height - data[i] * ratio;
+			if (i == 0) {
+				ctx.moveTo(x, y);
+			} else {
+				ctx.lineTo(x, y);		
+			}
+			console.log("p(" + x + "," + y + ")");
+			x = x + dx;    			
+		}
+		ctx.stroke();
+	    
+	    var tx = x - dx - 8;
+	    var ty = 26;
+	    var text = "" + data[data.length-1];
+	    console.log("Text: " +text+ "(" + tx + "," + ty + ")");
+	    ctx.lineWidth = 1.2;
+	    ctx.font = "14px tahoma";
+	    ctx.strokeStyle = "#000";
+	    ctx.fillStyle = "#f00";
+	    ctx.strokeText(text, tx, ty);
+
+	    var y = 0;
+	    var x = 0;
+		for (i in data) {
+			y = height - data[i] * ratio;
+			if (i == 4 || i == 6) {
+				tweetMarker(x, y);
+			}
+			x = x + dx;    			
+		}
+		
+	}
+
+	function tweetMarker(x, y) {
+		ctx.save();
+		ctx.strokeStyle = "#fff";
+	    ctx.fillStyle = "#a9f";
+
+	    ctx.beginPath();  
+	    ctx.arc(x, y, 4.2,0,Math.PI*2,true);
+	    ctx.closePath();
+	    ctx.stroke();
+	    ctx.fill();
+	    ctx.restore();
+	}
 });
 
-function draw(data) {
-
-}
